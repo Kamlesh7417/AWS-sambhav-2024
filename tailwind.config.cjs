0ed341317c284e58502d708b489dc70ccfc36fba
Kamlesh7417 <51952452+Kamlesh7417@users.noreply.github.com>
2024-11-23 14:50:17 +0530

    Update and rename tailwind.config.js to tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const forms = require('@tailwindcss/forms');
+
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -62,6 +64,6 @@ export default {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    forms,
   ],
-}
+};
